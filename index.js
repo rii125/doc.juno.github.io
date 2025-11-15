1e61d54a57e38fe20f2b2b67ee20485c8e7f82bb
rii_125 <rii125.idd@gmail.com>
2025-11-15 18:54:13 +0900

    Code to change title

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const lang = document.getElementById('lang')
 const title = document.getElementById('web-title')
 const blogClick = document.getElementById('blog-link')
 
+const titleList = [
+    'Juno | Currently under development...',
+    'Juno | Get started with \"Juno\"',
+    'Juno | Languages that work with Java',
+    'Juno | Wide range from beginners to advanced users'
+]
+
+let i = 0
+setInterval(() => {
+    document.title = titleList[i];
+    index = (index + 1) % titleList.length;
+}, 10000);
+
 // Blogの外部リンク
 function externalLink(message) {
     alert(message)
