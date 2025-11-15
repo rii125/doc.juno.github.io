702ca2922e6e56889a6ee025cfc20330bff448af
rii_125 <rii125.idd@gmail.com>
2025-11-15 18:35:06 +0900

    Create popup alert

---
+++ index.js
@@ -0,0 +1,13 @@
+'use Strict';
+
+const lang = document.getElementById('lang')
+const title = document.getElementById('web-title')
+const blogClick = document.getElementById('blog-link')
+
+// Blogの外部リンク
+function externalLink(message) {
+    alert(message)
+}
+function externalLink() {
+    alert('Move to external site (Japanese)');
+}
