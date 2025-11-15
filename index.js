7b0ab75f99f01c5c75a2a4be9c4c5d2c092fe549
rii_125 <rii125.idd@gmail.com>
2025-11-15 19:36:41 +0900

    Organizing code

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use Strict';
 
+// DOM variable
 const lang = document.getElementById('lang')
 const title = document.getElementById('web-title')
 const blogClick = document.getElementById('blog-link')
 const catchCopy = document.getElementById('catch-copy')
 
+// Array variable
 const titleList = [
     'Juno | Currently under development...',
     'Juno | Get started with \"Juno\"',
@@ -12,14 +14,21 @@ const titleList = [
     'Juno | Wide range from beginners to advanced users'
 ]
 
+// Const variable
+const changeMs = 1000  // 1000 = 1sec
+const sec = 3
+const titleCount = sec * changeMs
+
+// Can change variable
 let i = 0
+
 setInterval(() => {
     document.title = titleList[i]
     i = (i + 1) % titleList.length
     console.log(titleList[i])
-}, 10000)
+}, titleCount)
 
-// Blogの外部リンク
+// Blog external link
 function externalLink(message) {
     alert(message)
 }
